2f0cfb7e39526c7323c7120cb80abc253cf36a4b
Lucas Youngers <lucasyoungers@Adams-MacBook-Pro.local>
2019-03-21 12:22:46 -0700

    doesn't work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Active Martial Arts Logo" src="./assets/active-logo.png">
+    <ul class="AwardsList">
+      <award-card
+        v-for="award in awards"
+        v-bind:key="award.id"
+        v-bind:award="award" />
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AwardCard from "./components/AwardCard.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    AwardCard
+  },
+  data: function() {
+    awards: [
+      {
+        id: 1,
+        title: "High Kick",
+        detail: "Kick over your head 10 times in 10 seconds.",
+        imageurl: require("./assets/high-kick.jpg")
+      },
+      {
+        id: 2,
+        title: "Closed Fist",
+        detail: "Break a board with a closed fist.",
+        imageurl: require("./assets/closed-fist.jpg")
+      },
+      {
+        id: 3,
+        title: "Dragon Spirit",
+        detail: "Complete your form 100 times.",
+        imageurl: require("./assets/dragon-spirit.jpg")
+      }
+    ];
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: linear-gradient(rgba(200, 200, 200, 0.6), rgba(80, 80, 80, 0.7));
+}
+
+.AwardsList {
+  list-style-type: none;
 }
 </style>
